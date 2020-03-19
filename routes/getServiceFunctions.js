297e95b5d04699494385a79cfcbd01dcecab6e9a
Jose Andres Francisco <jose.andresf@stkbancomerans.com>
2020-03-19 13:29:15 -0600

    ValidateParamsBackendgetService v0.0.1

--- routes/getServiceFunctions.js
+++ routes/getServiceFunctions.js
@@ -1,5 +1,11 @@
 //función que valida el parámetro de entrada.
 function validateParam(param){
+	var RegExPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
+	if ((param.match(RegExPattern)) && (param!='')) {
+		return true;
+	} else {
+		return false;
+	}
 	
 }
 //Obtiene todos los registros del día seleccionado
@@ -9,8 +15,9 @@ function getRecords(){
 
 //Regresa todos los registros encontrados o el error en formato JSON
 function mainFunction(){
-    var prueba = 'Dato de prueba servicio Consulta ';
+   	var prueba = 'Dato de prueba servicio Consulta ';
 	return prueba;
+	
 }
 
 module.exports = {
