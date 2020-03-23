b2b80ba6efd995e8400de79475d62e97140d7142
Carlos_Pacheco <charlychav410@gmail.com>
2020-03-23 16:24:26 -0600

    completeService2 backend getService_getServiceFunction v0.0.2

--- routes/getService.js
+++ routes/getService.js
@@ -7,12 +7,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 
-router.get('/',(req,res, next)=>{
+router.post('/',(req,res, next)=>{
 	
 	var params = req.body;
 	var dateRegister = params.date;
-	var datoResponse = inFunctions.mainFunction(dateRegister);
-	res.send({datoResponse});
+	inFunctions.mainFunction(dateRegister).then(function(response){
+		res.send(response);
+	});
+		
 } );
 
 module.exports = router;
--- routes/getServiceFunctions.js
+++ routes/getServiceFunctions.js
@@ -1,6 +1,6 @@
 var dataBaseConection = require("./databaseConection.js");
 var mongoose = require('mongoose');
-//función que valida el parámetro de entrada.
+//Validate the param.
 function validateParam(param){
 	var RegExPattern = /^\d{1,2}\-\d{1,2}\-\d{2,4}$/;
 	if ((param.match(RegExPattern)) && (param!='')) {
@@ -11,6 +11,7 @@ function validateParam(param){
 	
 }
 
+//Generate the schema for database
 function createSchema(){
 	var createdSchema = mongoose.Schema({
 		IS: String,
@@ -21,40 +22,39 @@ function createSchema(){
 	return createdSchema;
 }
 
-//Obtiene todos los registros del día seleccionado
+//Get all the records of the selected day
 function getRecords(param){
-	var listResponse={};
-	var db = dataBaseConection.dataBaseConect();
+		var db = dataBaseConection.dataBaseConect();
+		var RegistrySchema = createSchema();
+		var Registry;
+		try{
+			Registry = mongoose.model('REGISTRY');
+		}catch(error){
+			Registry = mongoose.model('REGISTRY', RegistrySchema,'REGISTRY');
+		}
+			
+		var registry = {DATE:param};
+		db.on('error', console.error.bind(console, 'connection error:'));
 
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-	var RegistrySchema = createSchema();
-	var model = mongoose.model('REGISTRY', RegistrySchema,'REGISTRY');
-	console.log("Connection Successful!");
-
-	model.find({
-	}, (err, docs) => {
-	   if(err){
-		   console.log(err)
-	   } else{
-		 if(docs.length != 0){
-			listResponse=docs;
-			console.log(docs);
-		 }
-	   }
-	});
-});
- 	
-return listResponse;
-	
+		return new Promise(resolve=>{
+			db.once('open', function() {
+				console.log("Connection Successful!");
+				Registry.find(registry, function(err,res){
+					db.close(function(){
+						console.log("Connection close!");
+					});
+					resolve(res);
+				});
+			});
+		});	
+ 		
 }
 
 //Regresa todos los registros encontrados o el error en formato JSON
-function mainFunction(param){
+async function mainFunction(param){
 	var response = null;
-	
 	if(validateParam(param)){
-		var validateResponse = getRecords(param);
+		var validateResponse = await getRecords(param);
 		if(validateResponse==null){
 			response={code: "x002", description:"No hay registro" };
 		}else{
@@ -64,12 +64,13 @@ function mainFunction(param){
 	}else{
 		response={code: "x001", description:"Fecha invalida" };
 	}
-	return response;
+	return new Promise(resolve=>{
+		resolve( response);
+	});
+
 	
 }
 
 module.exports = {
-    "validateParam": validateParam,
-	"getRecords":getRecords,
 	"mainFunction":mainFunction
 }
